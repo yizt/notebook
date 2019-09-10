2bd1fdaf9a4310bd315622594c4824c52bd2923d
yizt <csuyzt@163.com>
2019-09-10 14:17:07 +0800

    增加检测样例图像

---
+++ computer_vision/detection/data_augment.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+   File Name：     data_augment.py
+   Description :  
+   Author :       mick.yi
+   Date：          2019/9/10
+"""
---
+++ computer_vision/detection/functional.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+   File Name：     functional.py
+   Description :  
+   Author :       mick.yi
+   Date：          2019/9/10
+"""
