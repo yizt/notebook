c1119c007d7a94e6a264d2e32d3fd5c3a1ded16c
yizt <csuyzt@163.com>
2019-09-10 14:43:58 +0800

    数据增广:增加pad

--- computer_vision/detection/data_augment.py
+++ computer_vision/detection/data_augment.py
@@ -5,11 +5,12 @@
    Author :       mick.yi
    Date：          2019/9/10
 """
-from torchvision.transforms import transforms
+from torchvision.transforms import transforms, functional as OF
 import functional as F
 import sys
 import collections
 from PIL import Image
+import numbers
 
 if sys.version_info < (3, 3):
     Sequence = collections.Sequence
@@ -77,4 +78,71 @@ class Resize(object):
             scale_h, scale_w = oh / h, ow / w
             boxes[:, ::2] *= scale_h
             boxes[:, 1::2] *= scale_w
+        return img, boxes, labels
+
+
+class Pad(object):
+    """Pad the given PIL Image on all sides with the given "pad" value.
+
+    Args:
+        padding (int or tuple): Padding on each border. If a single int is provided this
+            is used to pad all borders. If tuple of length 2 is provided this is the padding
+            on left/right and top/bottom respectively. If a tuple of length 4 is provided
+            this is the padding for the left, top, right and bottom borders
+            respectively.
+        fill (int or tuple): Pixel fill value for constant fill. Default is 0. If a tuple of
+            length 3, it is used to fill R, G, B channels respectively.
+            This value is only used when the padding_mode is constant
+        padding_mode (str): Type of padding. Should be: constant, edge, reflect or symmetric.
+            Default is constant.
+
+            - constant: pads with a constant value, this value is specified with fill
+
+            - edge: pads with the last value at the edge of the image
+
+            - reflect: pads with reflection of image without repeating the last value on the edge
+
+                For example, padding [1, 2, 3, 4] with 2 elements on both sides in reflect mode
+                will result in [3, 2, 1, 2, 3, 4, 3, 2]
+
+            - symmetric: pads with reflection of image repeating the last value on the edge
+
+                For example, padding [1, 2, 3, 4] with 2 elements on both sides in symmetric mode
+                will result in [2, 1, 1, 2, 3, 4, 4, 3]
+    """
+
+    def __init__(self, padding, fill=0, padding_mode='constant'):
+        assert isinstance(padding, (numbers.Number, tuple))
+        assert isinstance(fill, (numbers.Number, str, tuple))
+        assert padding_mode in ['constant', 'edge', 'reflect', 'symmetric']
+        if isinstance(padding, Sequence) and len(padding) not in [2, 4]:
+            raise ValueError("Padding must be an int or a 2, or 4 element tuple, not a " +
+                             "{} element tuple".format(len(padding)))
+
+        self.padding = padding
+        self.fill = fill
+        self.padding_mode = padding_mode
+
+        if isinstance(padding, numbers.Number):
+            self.pad_left = padding
+            self.pad_top = padding
+        elif isinstance(padding, Sequence) and len(padding) == 2:
+            self.pad_left, self.pad_top = padding
+        else:
+            self.pad_left, self.pad_top = padding[:2]
+
+    def __call__(self, img, boxes=None, labels=None):
+        """
+        Args:
+            img (PIL Image): Image to be padded.
+
+        Returns:
+            PIL Image: Padded image.
+        """
+        img = OF.pad(img, self.padding, self.fill, self.padding_mode)
+
+        if boxes is not None:
+            boxes = boxes.copy()
+            boxes[:, ::2] += self.pad_top
+            boxes[:, 1::2] += self.pad_left
         return img, boxes, labels
