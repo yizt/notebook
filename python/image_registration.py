87a1664edcf55688838c8666259073f265f65711
yizt <csuyzt@163.com>
2021-04-27 17:13:21 +0800

    add sth

--- python/image_registration.py
+++ python/image_registration.py
@@ -5,6 +5,8 @@
  @Author  : yizuotian
  @Description    :
 """
+import os
+
 import cv2
 import numpy as np
 
@@ -59,8 +61,75 @@ def perspective_transform(src_pts, matrix):
     dst_pts[:, 0] /= dst_pts[:, 2]
     dst_pts[:, 1] /= dst_pts[:, 2]
     dst_pts = dst_pts[:, :2]  # [N,(x,y)]
-    return dst_pts
+    return dst_pts.astype(np.int32)
 
 
+def draw_points(im, pts):
+    """
+
+    :param im:
+    :param pts: numpy [N,(x,y)]
+    :return:
+    """
+    for x, y in pts:
+        cv2.circle(im, (x, y), 3, (0, 0, 255), 3)
 
 
+def deal_im(src_pts, im_template, im):
+    # 查找匹配点，获取透视变换矩阵
+    _, _, matrix = find_correspondence_points(im_template, im)
+    dst_pts = perspective_transform(src_pts, matrix)
+
+    h, w = im.shape[:2]
+    im_warp = cv2.warpPerspective(im_template,
+                                  M=matrix,
+                                  dsize=(w, h))
+    im_dst = (0.5 * im + 0.5 * im_warp).astype(np.uint8)
+
+    # 画点
+    draw_points(im_dst, dst_pts)
+
+    if h < w:
+        return np.concatenate([im, im_dst], axis=0)
+    return np.concatenate([im, im_dst], axis=1)
+
+
+def main():
+    pts = [[58, 239],
+           [162, 342],
+           [264, 242],
+           [364, 342],
+           [464, 242],
+           [560, 340],
+           [658, 242],
+           [756, 340],
+           [62, 648],
+           [164, 746],
+           [268, 644],
+           [368, 742],
+           [468, 640],
+           [568, 736],
+           [664, 636],
+           [762, 732]]
+    pts = np.array(pts)
+    template_im_path = '/Volumes/Elements/土方智能工厂/template.jpg'
+    src_im_dir = '/Volumes/Elements/土方智能工厂/棋盘格'
+    out_dir = '/Volumes/Elements/土方智能工厂/match_output'
+
+    im_template = cv2.imread(template_im_path)
+    for im_name in os.listdir(src_im_dir):
+        print('deal {}'.format(im_name))
+        im_path = os.path.join(src_im_dir, im_name)
+        im = cv2.imread(im_path)
+
+        out_path = os.path.join(out_dir, im_name)
+
+        im_out = deal_im(pts, im_template, im)
+        cv2.imwrite(out_path, im_out)
+
+    # draw_points(im_template, pts)
+    # cv2.imwrite('/Volumes/Elements/土方智能工厂/template_draw.jpg', im_template)
+
+
+if __name__ == '__main__':
+    main()
