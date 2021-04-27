74db03406f644aeb476dd5c37ae7b518f6bf5659
yizt <csuyzt@163.com>
2021-04-27 13:42:33 +0800

    add sth

---
+++ python/image_registration.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+ @File    : image_registration.py
+ @Time    : 2021/4/27 上午8:40
+ @Author  : yizuotian
+ @Description    :
+"""
+import cv2
+import numpy as np
+
+
+def find_correspondence_points(img1, img2):
+    sift = cv2.xfeatures2d.SIFT_create()
+
+    # find the keypoints and descriptors with SIFT
+    kp1, des1 = sift.detectAndCompute(
+        cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY), None)
+    kp2, des2 = sift.detectAndCompute(
+        cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY), None)
+
+    # Find point matches
+    FLANN_INDEX_KDTREE = 0
+    index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+    search_params = dict(checks=50)
+    flann = cv2.FlannBasedMatcher(index_params, search_params)
+    matches = flann.knnMatch(des1, des2, k=2)
+
+    # Apply Lowe's SIFT matching ratio test
+    good = []
+    for m, n in matches:
+        if m.distance < 0.75 * n.distance:
+            good.append(m)
+
+    src_pts = np.asarray([kp1[m.queryIdx].pt for m in good])
+    dst_pts = np.asarray([kp2[m.trainIdx].pt for m in good])
+
+    # Constrain matches to fit homography
+    retval, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 20)
+    mask = mask.ravel()
+
+    # We select only inlier points
+    pts1 = src_pts[mask == 1]
+    pts2 = dst_pts[mask == 1]
+
+    return pts1.T, pts2.T, retval
+
+
+def perspective_transform(src_pts, matrix):
+    """
+    对坐标点透视变换
+    :param src_pts: 源坐标点 numpy [N,(x,y)]
+    :param matrix: 转换矩阵 numpy [3,3]
+    :return dst_pts: 目标坐标点 numpy [N,(x,y)]
+    """
+    num_points = src_pts.shape[0]
+    constant = np.ones(shape=(num_points, 1))
+    src_pts = np.concatenate([src_pts, constant], axis=1)  # [N,(x,y,1)]
+    dst_pts = np.dot(matrix, src_pts.T).T  # [N,(x,y,1)]
+    dst_pts[:, 0] /= dst_pts[:, 2]
+    dst_pts[:, 1] /= dst_pts[:, 2]
+    dst_pts = dst_pts[:, :2]  # [N,(x,y)]
+    return dst_pts
+
+
+
+
