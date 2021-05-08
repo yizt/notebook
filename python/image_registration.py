d2ea779a236874b387ce7c78b35af8296fa0b66d
yizt <csuyzt@163.com>
2021-05-08 11:35:15 +0800

    add sth

--- python/image_registration.py
+++ python/image_registration.py
@@ -5,6 +5,8 @@
  @Author  : yizuotian
  @Description    :
 """
+import colorsys
+import json
 import os
 
 import cv2
@@ -37,7 +39,9 @@ def find_correspondence_points(img1, img2):
     dst_pts = np.asarray([kp2[m.trainIdx].pt for m in good])
 
     # Constrain matches to fit homography
-    retval, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 20)
+    retval, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,
+                                      ransacReprojThreshold=5
+                                      )
     mask = mask.ravel()
 
     # We select only inlier points
@@ -76,6 +80,13 @@ def draw_points(im, pts):
 
 
 def deal_im(src_pts, im_template, im):
+    """
+
+    :param src_pts:
+    :param im_template:
+    :param im:
+    :return:
+    """
     # 查找匹配点，获取透视变换矩阵
     _, _, matrix = find_correspondence_points(im_template, im)
     dst_pts = perspective_transform(src_pts, matrix)
@@ -94,7 +105,117 @@ def deal_im(src_pts, im_template, im):
     return np.concatenate([im, im_dst], axis=1)
 
 
-def main():
+def random_colors(N, bright=True):
+    """
+    Generate random colors.
+    To get visually distinct colors, generate them in HSV space then
+    convert to RGB.
+    """
+    brightness = 1.0 if bright else 0.7
+    hsv = [(i / N, 1, brightness) for i in range(N)]
+    colors = [colorsys.hsv_to_rgb(h, s, v) for h, s, v in hsv]
+    # random.shuffle(colors)
+    colors = np.array(colors) * 255
+    return colors
+
+
+def draw_boxes(im, pts, colors):
+    """
+
+    :param im: [H,W,3]
+    :param pts: [N,4,(x,y)]
+    :param colors: [N,(B,G,R)]
+    :return:
+    """
+    im_mask = np.zeros_like(im)
+
+    for i, points in enumerate(pts):
+        color = colors[i]
+        cv2.fillConvexPoly(im_mask, points, color=color)
+
+    im_new = (im_mask * 0.5 + im * 0.5).astype(np.uint8)
+
+    mask = np.sum(im_mask, axis=-1)
+
+    return np.where(mask[:, :, np.newaxis] > 0, im_new, im)
+
+
+def parse_file(json_file):
+    """
+    解析vott标准
+    :param json_file:
+    :return:
+    """
+    with open(json_file, 'r', encoding='utf-8') as f:
+        ret_dic = json.load(f)
+        regions = list(ret_dic['assets'].values())[0]['regions']
+    pts = np.zeros(shape=(len(regions), 4, 2))  # [N,(lt,lr,br,bl),(x,y)]
+    for i, region in enumerate(regions):
+        for j, pt in enumerate(region['points']):
+            pts[i, j, 0] = pt['x']
+            pts[i, j, 1] = pt['y']
+
+    return pts
+
+
+def deal_im_valve(src_pts, im_template, im, colors):
+    """
+
+    :param src_pts: [N,4,2]
+    :param im_template: [H,W,3]
+    :param im: [H,W,3]
+    :param colors: [N,(B,G,R)]
+    :return:
+    """
+    # 查找匹配点，获取透视变换矩阵
+    _, _, matrix = find_correspondence_points(im_template, im)
+    dst_pts = perspective_transform(src_pts.reshape(-1, 2), matrix)
+    dst_pts = dst_pts.reshape(-1, 4, 2)
+    im_draw = draw_boxes(im, dst_pts, colors)
+    return im_draw
+
+
+def valve_main():
+    template_im_path = '/Volumes/Elements/土方智能工厂/工步防错-主阀/template.jpg'
+    src_im_dir = '/Volumes/Elements/土方智能工厂/工步防错-主阀/位置校准'
+    out_dir = '/Volumes/Elements/土方智能工厂/工步防错-主阀/位置校准_output_compare'
+    json_path = '/Volumes/Elements/土方智能工厂/工步防错-主阀/main_valve.json'
+
+    pts = parse_file(json_path)
+    colors = random_colors(len(pts))
+    im_template = cv2.imread(template_im_path)
+    for im_name in os.listdir(src_im_dir):
+        print('deal {}'.format(im_name))
+        im_path = os.path.join(src_im_dir, im_name)
+        im = cv2.imread(im_path)
+
+        out_path = os.path.join(out_dir, im_name)
+
+        im_out = deal_im_valve(pts, im_template, im, colors)
+        cv2.imwrite(out_path, im_out)
+
+
+def valve_main_2():
+    template_im_path = '/Volumes/Elements/土方智能工厂/工步防错-主阀/template.jpg'
+    src_im_dir = '/Volumes/Elements/土方智能工厂/工步防错-主阀/位置校准'
+    out_dir = '/Volumes/Elements/土方智能工厂/工步防错-主阀/位置校准_output_2'
+    json_path = '/Volumes/Elements/土方智能工厂/工步防错-主阀/main_valve.json'
+
+    pts = parse_file(json_path).reshape(-1,2)
+
+    im_template = cv2.imread(template_im_path)
+    for im_name in os.listdir(src_im_dir):
+        print('deal {}'.format(im_name))
+        im_path = os.path.join(src_im_dir, im_name)
+        im = cv2.imread(im_path)
+
+        out_path = os.path.join(out_dir, im_name)
+
+        im_out = deal_im(pts, im_template, im)
+        cv2.imwrite(out_path, im_out)
+
+
+def chess_main():
     pts = [[58, 239],
            [162, 342],
            [264, 242],
@@ -132,4 +253,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    valve_main()
